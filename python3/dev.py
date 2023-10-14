7a5b5cdcd5de80c83a859a2f52e8d7d9b0674fb7
theoXlol <146285549+theoXlol@users.noreply.github.com>
2023-10-14 13:30:35 +0200

    kapitel 11 in bearbeitung

--- python3/dev.py
+++ python3/dev.py
@@ -7,4 +7,23 @@ print(var1.lower())
 firstname = "theodor"
 lastname = "laukart"
 
-print ("hallo",firstname,lastname)
+print ("hallo",firstname,lastname) 
+
+
+########################################
+#         Datentypen                   #
+########################################
+
+print('Eins plus Zwei ist', 1 + 2)
+print('Fünf minus Drei ist', 5 - 3)
+print('Zwei hoch Drei ist',2 ** 3)
+print('Drei minus Vier ist', 3 - 4)
+print('Fünf mal Sechs sind', 5 * 6)
+print('Zwei geteilt durch Eins sind', 2 / 1)
+print('Der Rest von Fünf geteilt durch Vier ist', 5 % 4)
+print ('Zehn geteilt durch Zehn ist', 10 // 10)
+
+
+if 3<4:
+    print('Drei ist kleiner als Vier')
+    
