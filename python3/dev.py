2d25ba8e593def9eac5bb95b71be88db8f480e49
theoXlol <146285549+theoXlol@users.noreply.github.com>
2023-10-14 12:47:37 +0200

    initial

--- python3/dev.py
+++ python3/dev.py
@@ -1,1 +1,10 @@
-print("hallo Theodor")
+var1 = "hallo world"
+
+print(var1)
+print(var1.upper())
+print(var1.lower())
+
+firstname = "theodor"
+lastname = "laukart"
+
+print ("hallo",firstname,lastname)
