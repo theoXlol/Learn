6a162b4c2ddd528a996c60968a68eb73ecd374e4
theoXlol <146285549+theoXlol@users.noreply.github.com>
2023-12-17 13:23:53 +0100

    Initial

---
+++ Book_150_Challanges/1-11/004.py
@@ -0,0 +1,4 @@
+num1 = int(input("Enter First number :"))
+num2 = int(input("Enter Second number :"))
+
+print("The total is",num1+num2)
---
+++ Book_150_Challanges/1-11/005.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter First Number :"))
+num2 = int(input("Enter second number"))
+num3 = int(input("Enter Third number :"))
+answer = (num1 + num2) * num3
+
+print("The total is :", answer)
